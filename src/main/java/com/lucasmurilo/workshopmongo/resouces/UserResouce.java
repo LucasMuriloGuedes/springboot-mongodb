b4e832e39964b9d63791f6cd83b585ead8472163
LucasMuriloGuedes <lucasmurilo.guedes@gmail.com>
2021-05-11 12:48:07 -0400

    endpoint para retornar os posts de um usario

--- src/main/java/com/lucasmurilo/workshopmongo/resouces/UserResouce.java
+++ src/main/java/com/lucasmurilo/workshopmongo/resouces/UserResouce.java
@@ -1,5 +1,6 @@
 package com.lucasmurilo.workshopmongo.resouces;
 
+import com.lucasmurilo.workshopmongo.domain.Post;
 import com.lucasmurilo.workshopmongo.domain.User;
 import com.lucasmurilo.workshopmongo.dto.UserDTO;
 import com.lucasmurilo.workshopmongo.service.UserService;
@@ -52,5 +53,11 @@ public class UserResouce {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPost());
     }
 }
