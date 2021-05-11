3d302b21932659a130dc75e473c68f14a50a5035
LucasMuriloGuedes <lucasmurilo.guedes@gmail.com>
2021-05-11 15:10:04 -0400

    Consulta simples com @Query

--- src/main/java/com/lucasmurilo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lucasmurilo/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.lucasmurilo.workshopmongo.repository;
 import com.lucasmurilo.workshopmongo.domain.Post;
 import com.lucasmurilo.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,10 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{'title': {regex: ?0, $options: 'i'}}")
+    List<Post> searchTitle(String msg);
     List <Post> findByTitleContainingIgnoreCase(String text);
+
+
 
 }
